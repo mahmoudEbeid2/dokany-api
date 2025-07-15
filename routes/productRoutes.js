3d2642f409bccd387c493bd6dbd8e2ebe3c9718c
mohameddahroug <mhmeddahroug@gmail.com>
2025-07-15 18:26:43 +0300

    add images

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,30 +1,32 @@
 import express from "express";
+import upload from "../utils/multer.js";
+import {verifyToken} from "../middlewares/auth.js";
 import { addProduct, deleteProduct, getAllProducts, getDiscountedProductsBySeller, getProductById, getProductsBySellerId, getProductsBySubdomain, updateProduct } from "../controllers/productController.js";
 
 const router = express.Router();
 
 // Add product route
-router.post("/", addProduct);
+router.post("/",upload.array("images"),verifyToken, addProduct);
 
 // Get all products route
-router.get("/", getAllProducts);
+router.get("/", verifyToken, getAllProducts);
 
 // Get product by ID route
-router.get("/:id", getProductById);
+router.get("/:id", verifyToken, getProductById);
 
 // Get products by seller subdomain 
-router.get("/seller/subdomain/:subdomain", getProductsBySubdomain);
+router.get("/seller/subdomain/:subdomain", verifyToken, getProductsBySubdomain);
 
 // Get/products/seller/:subdomain/discount
-router.get("/seller/subdomain/:subdomain/discount", getDiscountedProductsBySeller);
+router.get("/seller/subdomain/:subdomain/discount", verifyToken, getDiscountedProductsBySeller);
 
 // Get products/seller/:sellerId
-router.get("/seller/:sellerId", getProductsBySellerId);
+router.get("/seller/:sellerId", verifyToken, getProductsBySellerId);
 
 // update product route
-router.put("/:id", updateProduct);
+router.put("/:id",upload.array("images"), verifyToken, updateProduct);
 
 // delete product route
-router.delete("/:id", deleteProduct);
+router.delete("/:id", verifyToken, deleteProduct);
 
 export default router;
