56d16daf29b1edb9a415eb7c40392a84a0f046c5
AbdelmonemMarei <abdomarey006@gmail.com>
2025-07-16 18:06:23 +0300

    Solve Seller register Logo Error by let multer to deal with several inputs not only profile_image

--- routes/auth/seller.auth.routes.js
+++ routes/auth/seller.auth.routes.js
@@ -4,7 +4,14 @@ import upload from '../../utils/multer.js';
 
 const router = Router();
 
-router.post('/register',upload.single('profile_imge'), register);
+router.post(
+  '/register',
+  upload.fields([
+    { name: 'profile_imge', maxCount: 1 },
+    { name: 'logo', maxCount: 1 },
+  ]),
+  register
+);
 router.post('/login', login);
 router.post('/reset-password', forgotPassword);
 router.post('/reset-password/confirm', resetPassword);
